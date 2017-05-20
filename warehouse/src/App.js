02677f55230a8b105d5570240bba78f98aea0967
focusjk <jiraphat.khupanit@gmail.com>
2017-05-21 00:11:27 +0700

    insert Navbar

--- warehouse/src/App.js
+++ warehouse/src/App.js
@@ -4,7 +4,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        Focuskuy
+
+        <nav className="navbar navbar-inverse">
+  			<div className="container-fluid">
+    			<div className="navbar-header">
+      				<a className="navbar-brand" href="#">WebSiteName</a>
+    			</div>
+    			
+    			<ul className="nav navbar-nav">
+      				<li className="active"><a href="#">Home</a></li>
+      				<li><a href="#">Page 1</a></li>
+      				<li><a href="#">Page 2</a></li>
+      				<li><a href="#">Page 3</a></li>
+    			</ul>
+  			</div>
+		</nav>
       </div>
     );
   }
